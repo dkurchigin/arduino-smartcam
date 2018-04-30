32a3371e198e42489b81023e09e698a354ad73b0
dkurchigin <kurchigin.dmitry@yandex.ru>
2018-04-30 19:38:23 +0300

    add moving functions

--- smartcam/smartcam.ino
+++ smartcam/smartcam.ino
@@ -1,48 +1,62 @@
 #include <Servo.h>
 
-#define LED_EYE 13
-#define LED_LIGHT 2
-#define OPTO A5
-
-int hb = 0;
-int vert_flag;
-
 Servo vertical_servo;
-int vert_pos = 0;
+Servo horizontal_servo;
 
 void setup() {
-  pinMode(LED_EYE, OUTPUT);
-  pinMode(LED_LIGHT, OUTPUT);
-  pinMode(OPTO, OUTPUT);
-  analogWrite(LED_EYE, 0);
-  digitalWrite(LED_LIGHT,1);
-  delay(100);
-  digitalWrite(LED_LIGHT,0);
-  vertical_servo.attach(9);
+  vertical_servo.attach(10);
+  horizontal_servo.attach(9);
+  vertical_servo.write(0);
+  delay(300);
+  horizontal_servo.write(0);
+  delay(300);
+  vertical_servo.write(90);
+  delay(300);
+  horizontal_servo.write(90);
+  delay(300);
+  vertical_servo.detach();
+  horizontal_servo.detach();
 }
 
 void loop() {
-  //digitalWrite(LED_EYE, hb);
-  //delay(3000);
-  //hb = 1;
-  //digitalWrite(LED_EYE, hb);
-  //delay(200);
-  //hb = 0;
-  vert_flag = analogRead(OPTO);
-  analogWrite(LED_EYE, vert_flag + 100);
-  //delay(vert_flag);
-  //analogWrite(LED_EYE, LOW);
-  //delay(vert_flag);
-  //analogWrite(LED_EYE, vert_flag);
+  //moveHorizontal("right");
+  //delay(100);
+  //moveHorizontal("left");
+  //delay(100);
+  //moveVertical("up");
+  //delay(100);
+  //moveVertical("down");
+  //delay(100);
+}
 
-  vertical_servo.write(0);
-  
-  //for (vert_pos = 0; vert_pos <= 180; vert_pos += 1) { 
-    //vertical_servo.write(vert_pos); 
-    //delay(3); 
-  //}
-  //for (vert_pos = 180; vert_pos >= 0; vert_pos -= 1) { 
-    //vertical_servo.write(vert_pos); 
-    //delay(3); 
-  //}
+void moveHorizontal(String direction) {
+  if (direction == "right") {
+    horizontal_servo.attach(9);
+    horizontal_servo.write(90);
+    delay(30);
+    horizontal_servo.detach();
+  } else if (direction == "left") {
+    horizontal_servo.attach(9);
+    horizontal_servo.write(180);
+    delay(30);
+    horizontal_servo.detach();
+  } else {
+    //nothing
+  }
+}
+
+void moveVertical(String direction) {
+  if (direction == "up") {
+    horizontal_servo.attach(10);
+    horizontal_servo.write(90);
+    delay(30);
+    horizontal_servo.detach();
+  } else if (direction == "down") {
+    horizontal_servo.attach(10);
+    horizontal_servo.write(180);
+    delay(30);
+    horizontal_servo.detach();
+  } else {
+    //nothing
+  }
 }
